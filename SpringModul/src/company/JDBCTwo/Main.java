6dd54ddc7cd145a0a9591218ff7509a1384d31bc
Serg <s.ustin.92@mail.ru>
2023-11-02 15:56:50 +0300

    add the second homework on JDBC

--- SpringModul/src/company/JDBCTwo/Main.java
+++ SpringModul/src/company/JDBCTwo/Main.java
@@ -3,59 +3,112 @@ package company.JDBCTwo;
 import java.sql.*;
 
 public class Main {
+    static String url = "jdbc:mysql://localhost:3306/top?userName=root&password=root";
+    static String user = "root";
+    static String pass = "root";
+    static Connection connection;
 
-//    public static void main(String[] args) {
-//        String url = "jdbc:mysql://localhost:3306/top";
-//        String user = "root";
-//        String pass = "root";
-//
-//        String query = "SELECT course_name,\n" +
-//                "       COUNT(course_name)/(MAX(MONTH(subscription_date)) - MIN(MONTH(subscription_date))) AS average_number_of_sales\n" +
-//                "FROM PurchaseList\n" +
-//                "GROUP BY course_name";
-//
-//        try (Connection connection = DriverManager.getConnection(url, user, pass);
-//             Statement statement = connection.createStatement()) {
-//            ResultSet resultSet = statement.executeQuery(query);
-//
-//            System.out.printf("|%-35s | %-10s |\n", "Название курса", "Среднее количество покупок");
-//            while (resultSet.next()) {
-//                System.out.printf("|%-35s | %-22s |\n",resultSet.getString("course_name"), resultSet.getString("average_number_of_sales"));
-//            }
-//
-//            resultSet.close();
-//
-//        } catch (Exception e) {
-//            e.printStackTrace();
-//        }
-//
-//    }
+    static {
+        try {
+            connection = DriverManager.getConnection(url, user, pass);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
 
     public static void main(String[] args) {
-        String url = "jdbc:mysql://localhost:3306/top?userName=root&password=root";
-        String user = "root";
-        String pass = "root";
+        try {
+            getSampleByTheHour();
+            addNewCourse("Scrum", 20, "MANAGEMENT",
+                    "Представляем вашему вниманию шикарный курс Scrum", 150, 20000);
+            changeThePriceOfTheCourseType(6000, "PROGRAMMING");
+//            deletingACourseByItsDuration(10);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
 
-        try (Connection connection = DriverManager.getConnection(url, user, pass)) {
+    }
 
-            String queryUpdate = "UPDATE courses set price = ? where type = 'PROGRAMMING'";
+    public static void getSampleByTheHour() throws SQLException {
+        Statement statement = connection.createStatement();
+        ResultSet resultSet = statement.executeQuery("SELECT name, duration, type FROM courses where duration >= 30");
 
-            try (PreparedStatement preparedStatement = connection.prepareStatement(queryUpdate)) {
+        System.out.printf("|%-34s|%-9s|%-12s|\n", "name", "duration", "type");
 
-                preparedStatement.setInt(1, 5000);
+        while (resultSet.next()) {
+            System.out.printf("|%-34s|%-9s|%-12s|\n", resultSet.getString("name"),
+                    resultSet.getString("duration"), resultSet.getString("type"));
+        }
 
-                int executeUpdate = preparedStatement.executeUpdate();
+        resultSet.close();
+        statement.close();
+    }
 
-                if (executeUpdate > 0) {
-                    System.out.println("Запись добавлена успешна");
-                } else {
-                    System.out.println("Произошла ошибка");
-                }
+    public static void addNewCourse(String name, int duration, String type, String description, int students_count, double price) {
+        String query = "INSERT INTO courses (name, duration, type, description, students_count, price, price_per_hour) " +
+                "VALUE (?,?,?,?,?,?,?)";
+        try (PreparedStatement preparedStatement = connection.prepareStatement(query)) {
+
+            preparedStatement.setString(1, name);
+            preparedStatement.setInt(2, duration);
+            preparedStatement.setString(3, type);
+            preparedStatement.setString(4, description);
+            preparedStatement.setInt(5, students_count);
+            preparedStatement.setDouble(6, price);
+            preparedStatement.setDouble(7, price / duration);
+
+            int executeUpdate = preparedStatement.executeUpdate();
+
+            if (executeUpdate > 0) {
+                System.out.println("Запись добавлена успешна");
+            } else {
+                System.out.println("Произошла ошибка");
             }
+
         } catch (SQLException e) {
             e.printStackTrace();
         }
+    }
 
+    public static void changeThePriceOfTheCourseType(int price, String type) {
+        String query = "UPDATE courses set price = ? where type = ?";
+        try (PreparedStatement preparedStatement = connection.prepareStatement(query)) {
+
+            preparedStatement.setInt(1, price);
+            preparedStatement.setString(2, type);
+
+            int executeUpdate = preparedStatement.executeUpdate();
+
+            if (executeUpdate > 0) {
+                System.out.println("Данные успешно изменены");
+            } else {
+                System.out.println("Произошла ошибка");
+            }
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void deletingACourseByItsDuration (int duration) {
+        String query = "DELETE FROM courses WHERE duration < ?";
+        try (PreparedStatement preparedStatement = connection.prepareStatement(query)) {
+
+            preparedStatement.setInt(1, duration);
+
+            int executeUpdate = preparedStatement.executeUpdate();
+
+            if (executeUpdate > 0) {
+                System.out.println("Данные успешно удалены");
+            } else {
+                System.out.println("Произошла ошибка");
+            }
+
+
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 
 }
