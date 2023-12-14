69ad4e80a7d0fa6b446715bc08db9a7c73700a4e
Serg <s.ustin.92@mail.ru>
2023-12-14 12:23:45 +0300

    add spring HW

--- SpringModul/src/company/Spring/SpringHW_01/src/main/java/com/company/Main.java
+++ SpringModul/src/company/Spring/SpringHW_01/src/main/java/com/company/Main.java
@@ -1,9 +1,10 @@
 package company.Spring.SpringHW_01.src.main.java.com.company;
 
-import com.company.config.AppConfig;
+
+import company.Spring.SpringHW_01.src.main.java.com.company.config.AppConfig;
+import company.Spring.SpringHW_01.src.main.java.com.company.service.NotificationService;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
-import com.company.service.NotificationService;
 
 public class Main {
     public static void main(String[] args) {
