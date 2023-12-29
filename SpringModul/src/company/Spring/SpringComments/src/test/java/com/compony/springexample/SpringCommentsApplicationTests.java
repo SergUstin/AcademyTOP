672c3b0ac1b5515c223a5dda091b874753b74397
Serg <s.ustin.92@mail.ru>
2023-12-29 12:38:59 +0300

    add SpringBoot Comments project

---
+++ SpringModul/src/company/Spring/SpringComments/src/test/java/com/compony/springexample/SpringCommentsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.compony.springexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCommentsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
